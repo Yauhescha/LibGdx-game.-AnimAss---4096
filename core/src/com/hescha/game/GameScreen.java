29cecf5658bde7091157a752638c39a4a37ac0cb
Yauhescha <evgescha.maslov007@yandex.ru>
2023-06-22 16:38:01 +0300

    add back menu

--- core/src/com/hescha/game/GameScreen.java
+++ core/src/com/hescha/game/GameScreen.java
@@ -4,6 +4,8 @@ import static com.hescha.game.LoadingScreen.WORLD_HEIGHT;
 import static com.hescha.game.LoadingScreen.WORLD_WIDTH;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputMultiplexer;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
@@ -14,6 +16,10 @@ import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.input.GestureDetector;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
@@ -36,9 +42,13 @@ public class GameScreen extends ScreenAdapter {
 
     private Game4096Service game4096Service;
     private Game4096 game4096;
+    TextButton exitButton;
 
+    private Stage stage;
     @Override
     public void show() {
+
+
         batch = new SpriteBatch();
         glyphLayout = new GlyphLayout();
         bitmapFont = new BitmapFont();
@@ -53,15 +63,41 @@ public class GameScreen extends ScreenAdapter {
 
         cellTextures = new TextureRegion[4][4];
         // Создание текстур для каждого значения в массиве
+
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
                 cellTextures[i][j] = createColoredTexture(getColorForValue(i));
             }
         }
-
-        Gdx.input.setInputProcessor(new GestureDetector(new MyGestureListener()));
         game4096Service = new Game4096Service();
         game4096 = game4096Service.newGame();
+
+        BitmapFont font = new BitmapFont();
+        font.getData().setScale(5f);
+
+        stage = new Stage(viewport);
+        TextButton.TextButtonStyle textButtonStyle = new TextButton.TextButtonStyle();
+        textButtonStyle.font = font;
+        textButtonStyle.fontColor = Color.WHITE;
+        exitButton = new TextButton("BACK", textButtonStyle);
+        exitButton.setPosition(0, WORLD_HEIGHT - exitButton.getHeight());
+
+        stage.addActor(exitButton);
+        exitButton.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                dispose();
+                GameFourZeroNineEight.game.setScreen(new MainMenuScreen());
+            }
+        });
+
+        InputProcessor myInputProcessor1 = new GestureDetector(new MyGestureListener());
+        InputProcessor myInputProcessor2 = stage;
+
+        InputMultiplexer multiplexer = new InputMultiplexer();
+        multiplexer.addProcessor(myInputProcessor1);
+        multiplexer.addProcessor(myInputProcessor2);
+        Gdx.input.setInputProcessor(multiplexer);
     }
 
     private TextureRegion createColoredTexture(Color color) {
@@ -129,7 +165,15 @@ public class GameScreen extends ScreenAdapter {
         glyphLayout.setText(bitmapFont, currentAction);
         bitmapFont.draw(batch, glyphLayout, 100, WORLD_HEIGHT - 100);
 
+        glyphLayout.setText(bitmapFont, "Score: " + game4096.getScore());
+        bitmapFont.draw(batch, glyphLayout,
+                WORLD_WIDTH - glyphLayout.width - 100,
+                WORLD_HEIGHT - glyphLayout.height - 100);
+
+
         batch.end();
+
+        stage.draw();
     }
 
     @Override
--- core/src/com/hescha/game/MainMenuScreen.java
+++ core/src/com/hescha/game/MainMenuScreen.java
@@ -1,5 +1,4 @@
 package com.hescha.game;
-import com.badlogic.gdx.Game;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
